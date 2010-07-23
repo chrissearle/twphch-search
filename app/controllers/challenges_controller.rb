1b7bab8b3861c5d576987b3da13a35ef8be1327f
Chris Searle <chris@chrissearle.org>
2010-07-23 17:59:56 +0200

    Page layout

--- app/controllers/challenges_controller.rb
+++ app/controllers/challenges_controller.rb
@@ -3,7 +3,11 @@ class ChallengesController < ApplicationController
     @challenge = Challenge.new
   end
 
-  def save
-    
+  def create
+    @challenge = Challenge.new(params[:challenge])
+
+    @info = flickr.photos.search(:group_id => ENV['FLICKR_GROUP'],
+                                 :tags => [@challenge.tag],
+                                 :extras => ["description, license, date_upload, date_taken, owner_name, icon_server, original_format, last_update, geo, tags, machine_tags, o_dims, views, media, path_alias, url_sq, url_t, url_s, url_m, url_o"])
   end
 end
