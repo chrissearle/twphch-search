26f9e9263a745667f1858941c8c8084d335c07c1
Chris Searle <chris@chrissearle.org>
2010-07-23 17:02:41 +0200

    Add basic model

---
+++ db/migrate/20100723150222_create_challenges.rb
@@ -0,0 +1,13 @@
+class CreateChallenges < ActiveRecord::Migration
+  def self.up
+    create_table :challenges do |t|
+      t.string :tag
+
+      t.timestamps
+    end
+  end
+
+  def self.down
+    drop_table :challenges
+  end
+end
